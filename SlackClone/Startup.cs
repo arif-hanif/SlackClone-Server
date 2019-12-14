8ac08e6c7658673d650111b1bde5dea20386c832
Arif Hanif <arif.hanif3@gmail.com>
2019-12-14 13:35:27 -0500

    add react app

--- SlackClone/Startup.cs
+++ SlackClone/Startup.cs
@@ -2,6 +2,7 @@ using HotChocolate;
 using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -61,10 +62,22 @@ namespace SlackClone
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSpaStaticFiles();
+
             // Adds GraphQL Service
             app.UseGraphQL();
             // Adds Playground IDE
             app.UsePlayground();
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
         }
     }
 }
