af8424e50691b2c2e34b34eec316920216203cc4
Arif Hanif <arif.hanif3@gmail.com>
2020-04-22 00:26:54 -0400

    add cors service

--- SlackClone/Startup.cs
+++ SlackClone/Startup.cs
@@ -37,7 +37,7 @@ namespace SlackClone
         // visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
 
             // configure jwt authentication
             services
